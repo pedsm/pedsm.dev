49aa2ee51aca88d6fbd4ee9ea27e2dc9cd29671c
Pedro Mendonca <peumendonca@gmail.com>
2020-03-22 18:37:26 +0000

    Add favicon

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,10 @@ export default class extends Document {
           <meta charSet="utf-8" />
           <meta http-equiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"></link>
           <link href="styles/main.css" rel="stylesheet" />
           <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet" />
         </Head>
