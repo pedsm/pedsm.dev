5e7cb88c93b8179b44c086f7452983f51bd6e17f
Pedro Mendonca <peumendonca@gmail.com>
2020-03-23 17:45:53 +0000

    Remove bulma link

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ export default function app({ Component, pageProps }) {
       <footer className="footer">
         <div className="content has-text-centered">
           <p>
-            <strong>pedsm.dev</strong> is built by <a href="https://github.com/pedsm">Pedro Mendonça</a> using <a href="https://bulma.io">Bulma</a>, <a href="https://nextjs.org/">Next.js</a> and deployed with <a href="https://zeit.co/home">Now</a>
+            <strong>pedsm.dev</strong> is built by <a href="https://github.com/pedsm">Pedro Mendonça</a>, using <a href="https://nextjs.org/">Next.js</a> and deployed with <a href="https://zeit.co/home">Now</a>
           </p>
         </div>
       </footer>
