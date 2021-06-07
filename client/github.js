3670f630d87d8b33199fe910399b4fa2055ff79f
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-06-07 21:27:01 +0100

    Fix?

--- client/github.js
+++ client/github.js
@@ -1,9 +1,11 @@
 export async function getRepo(name) {
-  const res = await fetch(`https://api.github.com/repos/pedsm/${name}`, {
-    headers: {
-      // TODO revoke this
-      Authorization: process.env.GH_AUTH
-    }
-  })
+  const res = await fetch(`https://api.github.com/repos/pedsm/${name}`, 
+    process.env.GH_AUTH 
+    ? {
+      headers: {
+        Authorization: process.env.GH_AUTH
+      }
+    } 
+  : {})
   return res.json() 
 }
