677e8c1f502f435f020ca2f1e94894e8177ab061
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-06-13 20:11:01 +0100

    New twitter card

--- pages/_app.js
+++ pages/_app.js
@@ -31,6 +31,7 @@ export default function app({ Component, pageProps }) {
           <meta property="og:image" content="/card.png" />
           <meta property="og:description" content="Hi, I'm Pedro" /> 
           <meta name="twitter:card" content="summary_large_image"></meta>
+          <meta name="twitter:image" content="/card.png" />
           <meta name="twitter:site" content="@pedsm"></meta>
           <meta name="twitter:creatro" content="@pedsm"></meta>
           <script type="text/javascript" src="startWorker.js"></script>
