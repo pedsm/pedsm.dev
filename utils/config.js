acdbfa31cd19075a64d882b36c799a222fd2fa02
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-07-18 15:54:28 +0100

    Update config.js

--- utils/config.js
+++ utils/config.js
@@ -2,7 +2,7 @@ export function getApiUrl() {
   if(isLocal()) {
     return `http://${process.env.VERCEL_URL}/`
   }
-  return `https://${process.env.VERCEL_URL}`
+  return `https://${process.env.VERCEL_URL}/`
 }
 
 export function isLocal() {
