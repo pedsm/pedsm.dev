de8e12796365a3762477f058c5ff164c3738f5cc
Pedro Santos de Mendonca <peumendonca@gmail.com>
2023-10-22 19:03:44 +0100

    Cleanup

--- pages/spook/index.tsx
+++ pages/spook/index.tsx
@@ -12,15 +12,22 @@ const selectOptions = options.map((a) => ({
 
 
 export default function SpookHome() {
-	const [selection, setSelection] = useState(selectOptions[0])
+	const [selection, setSelection] = useState<string | null>(null)
+	const [voter, setVoter] = useState<string | null>(null)
 
 	return (
 		<div className="spookMain">
 			<h1>Spooktacular Halloween 2023</h1>
 		
-			<select>
+			<select onChange={(e) => setSelection(e.target.value)}>
 				{options.map((a,i) => (
 					<option className="option" key={i}>{a.costume} ({a.name})</option>
+				))}
+			</select>
+			<br></br>
+			<select onChange={(e) => setSelection(e.target.value)}>
+				{options.map((a,i) => (
+					<option className="option" key={i}> ({a.name})</option>
 				))}
 			</select>
 		</div>
