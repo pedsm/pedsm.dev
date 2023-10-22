6f7650465d80f04feded20d7849f3339d37b7a4a
Pedro Santos de Mendonca <peumendonca@gmail.com>
2023-10-22 18:57:46 +0100

    a

--- pages/spook/index.tsx
+++ pages/spook/index.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import Select from 'react-select'
 
 const options = [
 	{ name: "Mika", costume: "Remy" },
@@ -18,18 +17,7 @@ export default function SpookHome() {
 	return (
 		<div className="spookMain">
 			<h1>Spooktacular Halloween 2023</h1>
-			<Select 
-				defaultValue={selectOptions[0]}
-				onChange={(newVal: {value: string, label: string}) => {
-					setSelection(newVal)
-					return
-				}}
-				options={selectOptions}
-				styles={{backgroundColor: 'orange'}}
-			/>
 		
-			<br></br>
-
 			<select>
 				{options.map((a,i) => (
 					<option className="option" key={i}>{a.costume} ({a.name})</option>
