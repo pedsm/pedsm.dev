eed041cb41bd4c8732acc649a37ed0e71ff174d6
Pedro Santos de Mendonca <peumendonca@gmail.com>
2023-10-27 20:43:50 +0100

    Remove themeing

--- pages/spook/index.tsx
+++ pages/spook/index.tsx
@@ -74,17 +74,17 @@ export default function SpookHome() {
 
 			<h1>Spooky Halloween 2023</h1>
 
-			<p>Best costume goes to:</p>
-			<select onChange={(e) => setSelection(e.target.value)}>
-				{options.map((a, i) => (
-					<option className="option" key={i} value={a.name}>{a.costume} ({a.name})</option>
-				))}
-			</select>
 			<p>My name is:</p>
 			<select onChange={(e) => setVoter(e.target.value)}>
 				<option className='option' value='none'>What's your name?</option>
 				{options.map((a, i) => (
 					<option className="option" key={i} value={a.name}> {a.name}</option>
+				))}
+			</select>
+			<p>Best costume goes to:</p>
+			<select onChange={(e) => setSelection(e.target.value)}>
+				{options.map((a, i) => (
+					<option className="option" key={i} value={a.name}>{a.costume} ({a.name})</option>
 				))}
 			</select>
 			{loading 
